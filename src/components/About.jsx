f1629c38320c11cb9378f980b2d080f884ba7309
SABHYA18 <sbhmhjn@gmail.com>
2024-02-22 23:56:21 +0530

    Halfway through the intro cards.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  const ServiceCard = ({ index, title, icon }) => {
+    return (
+      <Tilt className="xs:w-[250px] w-full">
+        <motion.div
+          variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+          className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+        >
+          <div
+            options={{
+              max: 45,
+              scale: 1,
+              speed: 450,
+            }}
+            className="bg-tertiary rounded-[20px] py-2 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+          >
+            <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+            <h3 className="text-white text-[20px] text-bold text-center">
+              {title}
+            </h3>
+          </div>
+        </motion.div>
+      </Tilt>
+    );
+  };
 
-export default About
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I&apos;m a committed JavaScript developer with experience with
+        frameworks like React and a solid foundation in backend programming. My
+        enthusiasm for technology is equaled only by my enthusiasm for football
+        and Star Wars, which inspire me to be creative and strive for
+        perfection. I do best in team contexts when communication between
+        members of the team is direct and intimate. I&apos;m excited to join
+        your team, add my expertise and passion, work on meaningful projects,
+        and promote a great work environment.
+      </motion.p>
+      {/* change the code in index.js in services */}
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default About;
